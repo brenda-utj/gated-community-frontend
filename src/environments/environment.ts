de805f274208829502fb2c9af7cc791e1cca67a9
Brenda Sotelo Diosdado <desarrollador.jr.pech@gmail.com>
2026-03-18 19:21:24 -0600

    fix: update apiUrl to include /api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api' // Tu backend de NestJS/Node
+    //apiUrl: 'http://localhost:3000/api' 
+    apiUrl: 'https://gated-community-backend-1ab7.onrender.com/api'
   };
