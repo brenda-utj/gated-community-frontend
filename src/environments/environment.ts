60b9939db8767afbfd847018f560eaa9ce00be06
Brenda Sotelo Diosdado <desarrollador.jr.pech@gmail.com>
2026-04-06 16:45:23 -0600

    se mejoró la landingpage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //apiUrl: 'http://localhost:3000/api' 
-    apiUrl: 'https://gated-community-backend-1ab7.onrender.com/api'
+    apiUrl: 'http://localhost:3000/api' 
+    //apiUrl: 'https://gated-community-backend-1ab7.onrender.com/api'
   };
